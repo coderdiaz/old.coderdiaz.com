971e433ac7aa051678c11ee7b615e4979750c735
Javier Diaz <coderdiaz@gmail.com>
2024-02-07 17:58:09 -0600

    fix: minor visual improvements

---
+++ src/components/Cursor.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from 'react';
+
+const OFFSET_CURSOR = 22;
+
+export default function Cursor() {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const links = document.querySelectorAll<HTMLAnchorElement>('a');
+    const cursor = document.getElementById('cursor') as HTMLDivElement;
+    cursor.style.transform = 'scale(1)';
+
+    const onMouseEnter = () => {
+      cursor.style.transform = 'scale(2)';
+    };
+
+    const onMouseLeave = () => {
+      cursor.style.transform = 'scale(1)';
+    };
+
+    const handleOnMouseMove = (event: MouseEvent) => {
+      const position = { x: event.clientX, y: event.clientY };
+      if (ref.current) {
+        ref.current.style.transform = `translateX(${position.x - OFFSET_CURSOR}px) translateY(${position.y - OFFSET_CURSOR}px) translateZ(0px)`;
+      }
+    };
+
+    document.addEventListener('mousemove', handleOnMouseMove);
+    links.forEach((link) => {
+      link.addEventListener('mouseenter', onMouseEnter);
+      link.addEventListener('mouseleave', onMouseLeave)
+    })
+    return () => {
+      document.removeEventListener('mousemove', handleOnMouseMove);
+      links.forEach((link) => {
+        link.removeEventListener('mouseenter', onMouseEnter);
+        link.removeEventListener('mouseleave', onMouseLeave)
+      })
+    }
+  }, []);
+
+  return (
+    <div ref={ref} className="hidden md:block fixed top-0 left-0 z-[99999] pointer-events-none transition-all duration-[.3s] ease-[cubic-bezier(.075,.82,.165,1)] will-change-transform origin-[center_center]">
+      <div id="cursor" className="absolute bg-[#FFB3C0]/20 w-10 h-10 rounded-full z-50 transform-none transition-all duration-[.3s] ease-[cubic-bezier(.075,.82,.165,1)]" />
+    </div>
+  );
+}
