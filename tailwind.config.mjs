ee28ee4b039e90fd8af3d9f6993998e387947512
Javier Diaz <coderdiaz@gmail.com>
2024-02-07 12:31:20 -0600

    feat: changed fonts in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,11 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      display: ['Safiro', 'ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      sans: ['Geist', 'ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New,monospace'],
+    },
     extend: {},
   },
   plugins: [],
