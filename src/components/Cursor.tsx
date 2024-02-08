9be1e467d5befb54f84d2c1cab156f61082be68f
Javier Diaz <coderdiaz@gmail.com>
2024-02-07 19:34:26 -0600

    fix: fixed mobile visual design

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -8,10 +8,12 @@ export default function Cursor() {
   useEffect(() => {
     const links = document.querySelectorAll<HTMLAnchorElement>('a');
     const cursor = document.getElementById('cursor') as HTMLDivElement;
+    const footer = document.getElementById('footer') as HTMLDivElement;
+    const services = document.getElementById('services') as HTMLDivElement;
     cursor.style.transform = 'scale(1)';
 
     const onMouseEnter = () => {
-      cursor.style.transform = 'scale(2)';
+      cursor.style.transform = 'scale(1.5)';
     };
 
     const onMouseLeave = () => {
@@ -25,17 +27,34 @@ export default function Cursor() {
       }
     };
 
+    const handleOnMouseEnter = () => {
+      cursor.style.backgroundColor = 'rgba(0, 0, 0, .2)';
+    }
+    const handleOnMouseLeave = () => {
+      cursor.style.backgroundColor = 'rgba(255, 179, 192, .2)';
+    }
+
     document.addEventListener('mousemove', handleOnMouseMove);
+    footer.addEventListener('mouseenter', handleOnMouseEnter);
+    footer.addEventListener('mouseleave', handleOnMouseLeave);
+    services.addEventListener('mouseenter', handleOnMouseEnter);
+    services.addEventListener('mouseleave', handleOnMouseLeave);
+
     links.forEach((link) => {
       link.addEventListener('mouseenter', onMouseEnter);
       link.addEventListener('mouseleave', onMouseLeave)
-    })
+    });
+
     return () => {
       document.removeEventListener('mousemove', handleOnMouseMove);
       links.forEach((link) => {
         link.removeEventListener('mouseenter', onMouseEnter);
-        link.removeEventListener('mouseleave', onMouseLeave)
+        link.removeEventListener('mouseleave', onMouseLeave);
       })
+      footer.removeEventListener('mouseenter', () => {});
+      footer.removeEventListener('mouseleave', () => {});
+      services.removeEventListener('mouseenter', () => {});
+      services.removeEventListener('mouseleave', () => {});
     }
   }, []);
 
