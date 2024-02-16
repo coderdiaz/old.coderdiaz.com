a47484c9987fe5592e99ea3794ff4aac4c2706d2
Javier Diaz <coderdiaz@gmail.com>
2024-02-16 14:00:55 -0600

    fix: improved cursor for projects

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -8,8 +8,10 @@ export default function Cursor() {
   useEffect(() => {
     const links = document.querySelectorAll<HTMLAnchorElement>('a');
     const cursor = document.getElementById('cursor') as HTMLDivElement;
+    const cursorProject = document.getElementById('cursor-project') as HTMLDivElement;
     const footer = document.getElementById('footer') as HTMLDivElement;
     const services = document.getElementById('services') as HTMLDivElement;
+    const projects = document.querySelectorAll<HTMLDivElement>('.project');
     cursor.style.transform = 'scale(1)';
 
     const onMouseEnter = () => {
@@ -48,6 +50,24 @@ export default function Cursor() {
       link.addEventListener('mouseleave', onMouseLeave)
     });
 
+    const onMouseEnterProject = (event: MouseEvent) => {
+      cursor.style.transform = 'scale(0)';
+      cursorProject.style.transform = 'scale(1)';
+      cursorProject.style.opacity = '1';
+      document.body.style.cursor = 'none';
+    }
+    const onMouseLeaveProject = (event: MouseEvent) => {
+      cursor.style.transform = 'scale(1)'
+      cursorProject.style.transform = 'scale(0)';
+      cursorProject.style.opacity = '0';
+      document.body.style.cursor = 'default';
+    }
+
+    projects.forEach((project) => {
+      project.addEventListener('mouseenter', onMouseEnterProject);
+      project.addEventListener('mouseleave', onMouseLeaveProject);
+    })
+
     return () => {
       document.removeEventListener('mousemove', handleOnMouseMove);
       links.forEach((link) => {
@@ -64,6 +84,9 @@ export default function Cursor() {
   return (
     <div ref={ref} className="hidden md:block fixed top-0 left-0 z-[99999] pointer-events-none transition-all duration-[.3s] ease-[cubic-bezier(.075,.82,.165,1)] will-change-transform origin-[center_center]">
       <div id="cursor" className="absolute bg-[#FFB3C0]/20 w-10 h-10 rounded-full z-[9999] transform-none transition-all duration-[.3s] ease-[cubic-bezier(.075,.82,.165,1)]" />
+      <div id="cursor-project" className="rounded-full -ml-[42px] -mt-0 text-white text-sm font-semibold flex items-center justify-center bg-black/85 backdrop-blur-md px-4 py-3 transform-none transition-all duration-[.4s] ease-[cubic-bezier(.325,.82,.350,1)] ring-2 ring-[#FFA36F]" style={{ transform: 'scale(0)', opacity: '0'}}>
+        <span className="font-sans">Ver proyecto</span>
+      </div>
     </div>
   );
 }
