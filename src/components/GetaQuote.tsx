63ebdb1eebe2a8fde83b9d3e1283c13493880ec0
Javier Diaz <coderdiaz@gmail.com>
2024-03-15 02:15:14 -0600

    feat: added animations on web

--- src/components/GetaQuote.tsx
+++ src/components/GetaQuote.tsx
@@ -7,7 +7,7 @@ export const GetaQuote = () => {
 
   return (
     <div className="flex items-center gap-2">
-      <button onClick={handleQuoteOpen} className="border-2 flex items-center gap-2.5 border-black font-medium w-max text-black rounded-xl text-sm px-4 py-2 bg-white transition-all duration-300 hover:ring-4 hover:ring-[#FFA36F40]">
+      <button onClick={handleQuoteOpen} className="border-2 flex items-center gap-2.5 border-black font-medium w-max text-black rounded-xl text-sm px-3 py-2 bg-white transition-all duration-300 hover:ring-4 hover:ring-[#FFA36F40]">
         Cotizar
       </button>
       <button aria-label="Menu principal" onClick={handleOpen} className="w-10 h-10 flex items-center justify-center border border-stone-700 rounded-xl p-2 text-white transition-all duration-300 hover:ring-4 hover:ring-[#FFA36F40]">
